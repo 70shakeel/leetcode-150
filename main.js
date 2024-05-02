62684e6b5739534178fa5baf3370ba31d647fae2
Shakeel <70shakeel@gmail.com>
2024-05-02 10:32:57 +0500

    Array/Strings: Trapping Rain Water

--- main.js
+++ main.js
@@ -1,46 +1,45 @@
-// Candy
+// Trapping Rain Water
 // Hard
 // Topics
 // Companies
-// There are n children standing in a line.Each child is assigned a rating value given in the integer array ratings.
-
-// You are giving candies to these children subjected to the following requirements:
-
-// Each child must have at least one candy.
-// Children with a higher rating get more candies than their neighbors.
-// Return the minimum number of candies you need to have to distribute the candies to the children.
+// Given n non - negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
 
 
 
 //     Example 1:
 
-// Input: ratings = [1, 0, 2]
-// Output: 5
-// Explanation: You can allocate to the first, second and third child with 2, 1, 2 candies respectively.
+
+// Input: height = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
+// Output: 6
+// Explanation: The above elevation map(black section) is represented by array[0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1].In this case, 6 units of rain water(blue section) are being trapped.
 //     Example 2:
 
-// Input: ratings = [1, 2, 2]
-// Output: 4
-// Explanation: You can allocate to the first, second and third child with 1, 2, 1 candies respectively.
-// The third child gets 1 candy because it satisfies the above two conditions.
-function minCandies(ratings) {
-    const n = ratings.length;
-    const candies = Array(n).fill(1);
+// Input: height = [4, 2, 0, 3, 2, 5]
+// Output: 9
+function trap(height) {
+    let left = 0;
+    let right = height.length - 1;
+    let leftMax = 0;
+    let rightMax = 0;
+    let result = 0;
 
-    // Left to right pass
-    for (let i = 1; i < n; i++) {
-        if (ratings[i] > ratings[i - 1]) {
-            candies[i] = candies[i - 1] + 1;
+    while (left < right) {
+        if (height[left] < height[right]) {
+            if (height[left] >= leftMax) {
+                leftMax = height[left];
+            } else {
+                result += leftMax - height[left];
+            }
+            left++;
+        } else {
+            if (height[right] >= rightMax) {
+                rightMax = height[right];
+            } else {
+                result += rightMax - height[right];
+            }
+            right--;
         }
     }
 
-    // Right to left pass
-    for (let i = n - 2; i >= 0; i--) {
-        if (ratings[i] > ratings[i + 1]) {
-            candies[i] = Math.max(candies[i], candies[i + 1] + 1);
-        }
-    }
-
-    // Sum up the candies
-    return candies.reduce((acc, val) => acc + val, 0);
+    return result;
 }
