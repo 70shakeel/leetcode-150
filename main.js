78c0a87f908f7571bb6efe41f1795301dcc5ab87
Shakeel <70shakeel@gmail.com>
2024-05-21 09:03:31 +0500

    Matrix: Rotate image

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-// Spiral Matrix
+// Rotate Image
 // Medium
 // Topics
 // Companies
-// Hint
-// Given an m x n matrix, return all elements of the matrix in spiral order.
+// You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees(clockwise).
+
+// You have to rotate the image in -place, which means you have to modify the input 2D matrix directly.DO NOT allocate another 2D matrix and do the rotation.
 
 
 
@@ -11,50 +12,26 @@
 
 
 // Input: matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-// Output: [1, 2, 3, 6, 9, 8, 7, 4, 5]
+// Output: [[7, 4, 1], [8, 5, 2], [9, 6, 3]]
 // Example 2:
 
 
-// Input: matrix = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]
-// Output: [1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7]
-function spiralOrder(matrix) {
-    if (matrix.length === 0) return [];
+// Input: matrix = [[5, 1, 9, 11], [2, 4, 8, 10], [13, 3, 6, 7], [15, 14, 12, 16]]
+// Output: [[15, 13, 2, 5], [14, 3, 4, 1], [12, 6, 8, 9], [16, 7, 10, 11]]
+function rotate(matrix) {
+    const n = matrix.length;
 
-    const result = [];
-    let top = 0;
-    let bottom = matrix.length - 1;
-    let left = 0;
-    let right = matrix[0].length - 1;
-
-    while (top <= bottom && left <= right) {
-        // Traverse from left to right along the top boundary
-        for (let i = left; i <= right; i++) {
-            result.push(matrix[top][i]);
-        }
-        top++;
-
-        // Traverse from top to bottom along the right boundary
-        for (let i = top; i <= bottom; i++) {
-            result.push(matrix[i][right]);
-        }
-        right--;
-
-        if (top <= bottom) {
-            // Traverse from right to left along the bottom boundary
-            for (let i = right; i >= left; i--) {
-                result.push(matrix[bottom][i]);
-            }
-            bottom--;
-        }
-
-        if (left <= right) {
-            // Traverse from bottom to top along the left boundary
-            for (let i = bottom; i >= top; i--) {
-                result.push(matrix[i][left]);
-            }
-            left++;
+    // Transpose the matrix
+    for (let i = 0; i < n; i++) {
+        for (let j = i; j < n; j++) {
+            [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
         }
     }
 
-    return result;
+    // Reverse each row
+    for (let i = 0; i < n; i++) {
+        matrix[i].reverse();
+    }
+
+    return matrix;
 }
