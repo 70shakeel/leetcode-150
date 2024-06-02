a132d112bdd2c5895364dd66da9fbed6d593954c
Shakeel <70shakeel@gmail.com>
2024-06-02 15:24:03 +0500

    Intervals: Summary Ranges

--- main.js
+++ main.js
@@ -1,43 +1,53 @@
-// Longest Consecutive Sequence
-// Medium
+// Summary Ranges
+// Easy
 // Topics
 // Companies
-// Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+// You are given a sorted unique integer array nums.
 
-// You must write an algorithm that runs in O(n) time.
+// A range[a, b] is the set of all integers from a to b(inclusive).
+
+// Return the smallest sorted list of ranges that cover all the numbers in the array exactly.That is, each element of nums is covered by exactly one of the ranges, and there is no integer x such that x is in one of the ranges but not in nums.
+
+// Each range[a, b] in the list should be output as:
+
+// "a->b" if a != b
+// "a" if a == b
 
 
+// Example 1:
 
-//     Example 1:
-
-// Input: nums = [100, 4, 200, 1, 3, 2]
-// Output: 4
-// Explanation: The longest consecutive elements sequence is[1, 2, 3, 4].Therefore its length is 4.
+// Input: nums = [0, 1, 2, 4, 5, 7]
+// Output: ["0->2", "4->5", "7"]
+// Explanation: The ranges are:
+// [0, 2]-- > "0->2"
+// [4, 5]-- > "4->5"
+// [7, 7]-- > "7"
 // Example 2:
 
-// Input: nums = [0, 3, 7, 2, 5, 8, 4, 6, 0, 1]
-// Output: 9
-function longestConsecutive(nums) {
-    if (nums.length === 0) return 0;
+// Input: nums = [0, 2, 3, 4, 6, 8, 9]
+// Output: ["0", "2->4", "6", "8->9"]
+// Explanation: The ranges are:
+// [0, 0]-- > "0"
+// [2, 4]-- > "2->4"
+// [6, 6]-- > "6"
+// [8, 9]-- > "8->9"
+function summaryRanges(nums) {
+    const result = [];
+    let i = 0;
 
-    const numSet = new Set(nums);
-    let maxLength = 0;
-
-    for (let num of numSet) {
-        // Check if it's the start of a sequence
-        if (!numSet.has(num - 1)) {
-            let currentNum = num;
-            let currentStreak = 1;
-
-            // Check the length of the sequence
-            while (numSet.has(currentNum + 1)) {
-                currentNum += 1;
-                currentStreak += 1;
-            }
-
-            maxLength = Math.max(maxLength, currentStreak);
+    while (i < nums.length) {
+        let start = nums[i];
+        while (i < nums.length - 1 && nums[i] + 1 === nums[i + 1]) {
+            i++;
         }
+        let end = nums[i];
+        if (start === end) {
+            result.push(`${start}`);
+        } else {
+            result.push(`${start}->${end}`);
+        }
+        i++;
     }
 
-    return maxLength;
+    return result;
 }
