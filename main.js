d3e97e243da6a49e192923e806e0f1ba8ddce97a
Shakeel <70shakeel@gmail.com>
2024-06-19 15:21:03 +0500

    Linked list: partition list

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
-// Rotate List
+// Partition List
 // Medium
 // Topics
 // Companies
-// Given the head of a linked list, rotate the list to the right by k places.
+// Given the head of a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
+
+// You should preserve the original relative order of the nodes in each of the two partitions.
 
 
 
 //     Example 1:
 
 
-// Input: head = [1, 2, 3, 4, 5], k = 2
-// Output: [4, 5, 1, 2, 3]
+// Input: head = [1, 4, 3, 2, 5, 2], x = 3
+// Output: [1, 2, 2, 4, 3, 5]
 // Example 2:
 
-
-// Input: head = [0, 1, 2], k = 4
-// Output: [2, 0, 1]
+// Input: head = [2, 1], x = 2
+// Output: [1, 2]
 class ListNode {
     constructor(val = 0, next = null) {
         this.val = val;
@@ -23,37 +24,39 @@ class ListNode {
     }
 }
 
-function rotateRight(head, k) {
-    if (!head || !head.next || k === 0) return head;
+function partition(head, x) {
+    // Create two dummy nodes
+    let lessHead = new ListNode(0);
+    let greaterHead = new ListNode(0);
 
-    // Find the length of the linked list
-    let length = 1;
-    let tail = head;
-    while (tail.next) {
-        tail = tail.next;
-        length++;
+    // Create two pointers to the current node in each partition
+    let less = lessHead;
+    let greater = greaterHead;
+
+    // Traverse the original list
+    while (head !== null) {
+        if (head.val < x) {
+            less.next = head;
+            less = less.next;
+        } else {
+            greater.next = head;
+            greater = greater.next;
+        }
+        head = head.next;
     }
 
-    // Calculate the effective number of rotations
-    k = k % length;
-    if (k === 0) return head;
+    // End the greater list
+    greater.next = null;
 
-    // Find the new tail (length - k - 1) and new head (length - k)
-    let newTail = head;
-    for (let i = 0; i < length - k - 1; i++) {
-        newTail = newTail.next;
-    }
-    let newHead = newTail.next;
+    // Attach the greater list to the end of the less list
+    less.next = greaterHead.next;
 
-    // Rotate the list
-    newTail.next = null;
-    tail.next = head;
-
-    return newHead;
+    // Return the head of the new list, which is lessHead.next
+    return lessHead.next;
 }
 
-// Helper function to convert array to linked list
-function arrayToList(arr) {
+// Helper function to create a linked list from an array
+function createLinkedList(arr) {
     let dummy = new ListNode();
     let current = dummy;
     for (let val of arr) {
@@ -63,10 +66,10 @@ function arrayToList(arr) {
     return dummy.next;
 }
 
-// Helper function to convert linked list to array
-function listToArray(head) {
+// Helper function to convert a linked list to an array
+function linkedListToArray(head) {
     let arr = [];
-    while (head) {
+    while (head !== null) {
         arr.push(head.val);
         head = head.next;
     }
