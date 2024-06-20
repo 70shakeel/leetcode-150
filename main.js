1af807ff6dc12a618c9228c6d2c8db446f092bcd
Shakeel <70shakeel@gmail.com>
2024-06-20 09:32:49 +0500

    Linked list: LRU Cache

--- main.js
+++ main.js
@@ -1,77 +1,109 @@
-// Partition List
+// LRU Cache
 // Medium
 // Topics
 // Companies
-// Given the head of a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
+// Design a data structure that follows the constraints of a Least Recently Used(LRU) cache.
 
-// You should preserve the original relative order of the nodes in each of the two partitions.
+// Implement the LRUCache class:
+
+// LRUCache(int capacity) Initialize the LRU cache with positive size capacity.
+// int get(int key) Return the value of the key if the key exists, otherwise return -1.
+// void put(int key, int value) Update the value of the key if the key exists.Otherwise, add the key - value pair to the cache.If the number of keys exceeds the capacity from this operation, evict the least recently used key.
+// The functions get and put must each run in O(1) average time complexity.
 
 
 
 //     Example 1:
 
+// Input
+// ["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
+// [[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
+// Output
+// [null, null, null, 1, null, -1, null, -1, 3, 4]
 
-// Input: head = [1, 4, 3, 2, 5, 2], x = 3
-// Output: [1, 2, 2, 4, 3, 5]
-// Example 2:
-
-// Input: head = [2, 1], x = 2
-// Output: [1, 2]
-class ListNode {
-    constructor(val = 0, next = null) {
-        this.val = val;
-        this.next = next;
+// Explanation
+// LRUCache lRUCache = new LRUCache(2);
+// lRUCache.put(1, 1); // cache is {1=1}
+// lRUCache.put(2, 2); // cache is {1=1, 2=2}
+// lRUCache.get(1);    // return 1
+// lRUCache.put(3, 3); // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+// lRUCache.get(2);    // returns -1 (not found)
+// lRUCache.put(4, 4); // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+// lRUCache.get(1);    // return -1 (not found)
+// lRUCache.get(3);    // return 3
+// lRUCache.get(4);    // return 4
+class Node {
+    constructor(key, value) {
+        this.key = key;
+        this.value = value;
+        this.prev = null;
+        this.next = null;
     }
 }
 
-function partition(head, x) {
-    // Create two dummy nodes
-    let lessHead = new ListNode(0);
-    let greaterHead = new ListNode(0);
+class DoublyLinkedList {
+    constructor() {
+        this.head = new Node(null, null);
+        this.tail = new Node(null, null);
+        this.head.next = this.tail;
+        this.tail.prev = this.head;
+    }
 
-    // Create two pointers to the current node in each partition
-    let less = lessHead;
-    let greater = greaterHead;
+    addNodeToFront(node) {
+        node.next = this.head.next;
+        node.prev = this.head;
+        this.head.next.prev = node;
+        this.head.next = node;
+    }
 
-    // Traverse the original list
-    while (head !== null) {
-        if (head.val < x) {
-            less.next = head;
-            less = less.next;
-        } else {
-            greater.next = head;
-            greater = greater.next;
+    removeNode(node) {
+        const prevNode = node.prev;
+        const nextNode = node.next;
+        prevNode.next = nextNode;
+        nextNode.prev = prevNode;
+    }
+
+    moveToFront(node) {
+        this.removeNode(node);
+        this.addNodeToFront(node);
+    }
+
+    removeLRUNode() {
+        const lruNode = this.tail.prev;
+        this.removeNode(lruNode);
+        return lruNode;
+    }
+}
+
+class LRUCache {
+    constructor(capacity) {
+        this.capacity = capacity;
+        this.map = new Map();
+        this.list = new DoublyLinkedList();
+    }
+
+    get(key) {
+        if (!this.map.has(key)) {
+            return -1;
         }
-        head = head.next;
+        const node = this.map.get(key);
+        this.list.moveToFront(node);
+        return node.value;
     }
 
-    // End the greater list
-    greater.next = null;
-
-    // Attach the greater list to the end of the less list
-    less.next = greaterHead.next;
-
-    // Return the head of the new list, which is lessHead.next
-    return lessHead.next;
-}
-
-// Helper function to create a linked list from an array
-function createLinkedList(arr) {
-    let dummy = new ListNode();
-    let current = dummy;
-    for (let val of arr) {
-        current.next = new ListNode(val);
-        current = current.next;
+    put(key, value) {
+        if (this.map.has(key)) {
+            const node = this.map.get(key);
+            node.value = value;
+            this.list.moveToFront(node);
+        } else {
+            if (this.map.size >= this.capacity) {
+                const lruNode = this.list.removeLRUNode();
+                this.map.delete(lruNode.key);
+            }
+            const newNode = new Node(key, value);
+            this.list.addNodeToFront(newNode);
+            this.map.set(key, newNode);
+        }
     }
-    return dummy.next;
-}
-
-// Helper function to convert a linked list to an array
-function linkedListToArray(head) {
-    let arr = [];
-    while (head !== null) {
-        arr.push(head.val);
-        head = head.next;
-    }
-    return arr;
 }
