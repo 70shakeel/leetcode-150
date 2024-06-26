52effe067e4d14770af344d3ea1aaf37c7cec803
Shakeel <70shakeel@gmail.com>
2024-06-26 10:24:46 +0500

    Linked list: Populating Next Right Pointers in Each Node II

--- main.js
+++ main.js
@@ -1,64 +1,102 @@
-// Construct Binary Tree from Inorder and Postorder Traversal
+// Populating Next Right Pointers in Each Node II
 // Medium
 // Topics
 // Companies
-// Given two integer arrays inorder and postorder where inorder is the inorder traversal of a binary tree and postorder is the postorder traversal of the same tree, construct and return the binary tree.
+// Given a binary tree
+
+// struct Node {
+//   int val;
+//     Node * left;
+//     Node * right;
+//     Node * next;
+// }
+// Populate each next pointer to point to its next right node.If there is no next right node, the next pointer should be set to NULL.
+
+//     Initially, all next pointers are set to NULL.
 
 
 
-//     Example 1:
+//         Example 1:
 
 
-// Input: inorder = [9, 3, 15, 20, 7], postorder = [9, 15, 7, 20, 3]
-// Output: [3, 9, 20, null, null, 15, 7]
-// Example 2:
+// Input: root = [1, 2, 3, 4, 5, null, 7]
+// Output: [1,#, 2, 3,#, 4, 5, 7,#]
+// Explanation: Given the above binary tree(Figure A), your function should populate each next pointer to point to its next right node, just like in Figure B.The serialized output is in level order as connected by the next pointers, with '#' signifying the end of each level.
+//     Example 2:
 
-// Input: inorder = [-1], postorder = [-1]
-// Output: [-1]
-
-
-// Constraints:
-
-// 1 <= inorder.length <= 3000
-// postorder.length == inorder.length
-//     - 3000 <= inorder[i], postorder[i] <= 3000
-// inorder and postorder consist of unique values.
-// Each value of postorder also appears in inorder.
-// inorder is guaranteed to be the inorder traversal of the tree.
-// postorder is guaranteed to be the postorder traversal of the tree.
-class TreeNode {
-    constructor(val) {
+// Input: root = []
+// Output: []
+// Define the Node structure
+class Node {
+    constructor(val, left = null, right = null, next = null) {
         this.val = val;
-        this.left = null;
-        this.right = null;
+        this.left = left;
+        this.right = right;
+        this.next = next;
     }
 }
 
-function buildTree(inorder, postorder) {
-    // Map to store the index of each value in inorder traversal
-    const inorderIndexMap = new Map();
-    inorder.forEach((val, index) => inorderIndexMap.set(val, index));
+// Function to populate each next pointer to point to its next right node
+function connect(root) {
+    if (!root) return null;
 
-    // Recursive helper function
-    function helper(inLeft, inRight) {
-        // If there are no elements to construct the tree
-        if (inLeft > inRight) return null;
+    let queue = [root];
 
-        // Pick up the last element as a root
-        const rootVal = postorder.pop();
-        const root = new TreeNode(rootVal);
+    while (queue.length > 0) {
+        let size = queue.length;
+        let prev = null;
 
-        // Root splits inorder list into left and right subtrees
-        const index = inorderIndexMap.get(rootVal);
+        for (let i = 0; i < size; i++) {
+            let currentNode = queue.shift();
 
-        // Build right subtree
-        root.right = helper(index + 1, inRight);
-        // Build left subtree
-        root.left = helper(inLeft, index - 1);
+            if (prev) {
+                prev.next = currentNode;
+            }
+            prev = currentNode;
 
-        return root;
+            if (currentNode.left) queue.push(currentNode.left);
+            if (currentNode.right) queue.push(currentNode.right);
+        }
+        // At the end of the level, make sure the last node's next is set to null
+        if (prev) {
+            prev.next = null;
+        }
     }
 
-    // Start from the entire range of inorder array
-    return helper(0, inorder.length - 1);
+    return root;
+}
+
+// Helper function to print the tree level by level using next pointers
+function printTreeByNext(root) {
+    if (!root) return;
+
+    let start = root;
+
+    while (start) {
+        let current = start;
+        let level = '';
+        while (current) {
+            level += current.val + ' -> ';
+            current = current.next;
+        }
+        console.log(level + 'null');
+
+        // Move to the next level
+        if (start.left) {
+            start = start.left;
+        } else if (start.right) {
+            start = start.right;
+        } else {
+            // Find the next starting node from the next pointers
+            let nextStart = start.next;
+            while (nextStart && !nextStart.left && !nextStart.right) {
+                nextStart = nextStart.next;
+            }
+            if (nextStart) {
+                start = nextStart.left ? nextStart.left : nextStart.right;
+            } else {
+                start = null;
+            }
+        }
+    }
 }
