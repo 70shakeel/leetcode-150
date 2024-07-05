3e6b1fbe2fdeb964d3477a46035f7264eaab4ceb
Shakeel <70shakeel@gmail.com>
2024-07-05 10:15:20 +0500

    Binary Tree BFS: Average of Levels in Binary Tree

--- main.js
+++ main.js
@@ -1,53 +1,49 @@
-// Binary Tree Right Side View
-// Solved
-// Medium
+// Average of Levels in Binary Tree
+// Easy
 // Topics
 // Companies
-// Given the root of a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
-
+// Given the root of a binary tree, return the average value of the nodes on each level in the form of an array.Answers within 10 - 5 of the actual answer will be accepted.
 
 
 //     Example 1:
 
 
-// Input: root = [1, 2, 3, null, 5, null, 4]
-// Output: [1, 3, 4]
-// Example 2:
+// Input: root = [3, 9, 20, null, null, 15, 7]
+// Output: [3.00000, 14.50000, 11.00000]
+// Explanation: The average value of nodes on level 0 is 3, on level 1 is 14.5, and on level 2 is 11.
+// Hence return [3, 14.5, 11].
+//     Example 2:
 
-// Input: root = [1, null, 3]
-// Output: [1, 3]
-// Example 3:
 
-// Input: root = []
-// Output: []
-class TreeNode {
-    constructor(val = 0, left = null, right = null) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
+// Input: root = [3, 9, 20, 15, 7]
+// Output: [3.00000, 14.50000, 11.00000]
+// Definition for a binary tree node.
+function TreeNode(val, left, right) {
+    this.val = (val === undefined ? 0 : val);
+    this.left = (left === undefined ? null : left);
+    this.right = (right === undefined ? null : right);
 }
 
-var rightSideView = function (root) {
+function averageOfLevels(root) {
     if (!root) return [];
 
-    const result = [];
+    const averages = [];
     const queue = [root];
 
     while (queue.length > 0) {
         const levelSize = queue.length;
-        let lastNodeValue = null;
+        let levelSum = 0;
 
         for (let i = 0; i < levelSize; i++) {
             const node = queue.shift();
-            lastNodeValue = node.val;
+            levelSum += node.val;
 
             if (node.left) queue.push(node.left);
             if (node.right) queue.push(node.right);
         }
 
-        result.push(lastNodeValue);
+        averages.push(levelSum / levelSize);
     }
 
-    return result;
-};
+    return averages;
+}
