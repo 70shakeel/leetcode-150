1e81631f8064c46b7620408329533f8c8e25bac6
Shakeel <70shakeel@gmail.com>
2024-08-06 11:36:35 +0500

    Binary Search: Search a 2D Matrix

--- main.js
+++ main.js
@@ -1,40 +1,49 @@
-// Search Insert Position
-// Easy
+// Search a 2D Matrix
+// Medium
 // Topics
 // Companies
-// Given a sorted array of distinct integers and a target value, return the index if the target is found.If not, return the index where it would be if it were inserted in order.
+// You are given an m x n integer matrix matrix with the following two properties:
 
-// You must write an algorithm with O(log n) runtime complexity.
+// Each row is sorted in non - decreasing order.
+// The first integer of each row is greater than the last integer of the previous row.
+// Given an integer target, return true if target is in matrix or false otherwise.
+
+// You must write a solution in O(log(m * n)) time complexity.
 
 
 
 //     Example 1:
 
-// Input: nums = [1, 3, 5, 6], target = 5
-// Output: 2
+
+// Input: matrix = [[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], target = 3
+// Output: true
 // Example 2:
 
-// Input: nums = [1, 3, 5, 6], target = 2
-// Output: 1
-// Example 3:
 
-// Input: nums = [1, 3, 5, 6], target = 7
-// Output: 4
-function searchInsert(nums, target) {
+// Input: matrix = [[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], target = 13
+// Output: false
+function searchMatrix(matrix, target) {
+    if (!matrix.length || !matrix[0].length) {
+        return false;
+    }
+
+    let m = matrix.length;
+    let n = matrix[0].length;
     let left = 0;
-    let right = nums.length - 1;
+    let right = m * n - 1;
 
     while (left <= right) {
         let mid = Math.floor((left + right) / 2);
+        let midValue = matrix[Math.floor(mid / n)][mid % n];
 
-        if (nums[mid] === target) {
-            return mid;
-        } else if (nums[mid] < target) {
+        if (midValue === target) {
+            return true;
+        } else if (midValue < target) {
             left = mid + 1;
         } else {
             right = mid - 1;
         }
     }
 
-    return left;
+    return false;
 }
