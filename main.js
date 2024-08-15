60821a8fc54e5de79ab0ef13b1ffd90cdc9fae9f
Shakeel <70shakeel@gmail.com>
2024-08-15 11:37:22 +0500

    Heap: Find Median from Data Stream

--- main.js
+++ main.js
@@ -1,56 +1,109 @@
-// Find K Pairs with Smallest Sums
-// Medium
+// Find Median from Data Stream
+// Hard
 // Topics
 // Companies
-// You are given two integer arrays nums1 and nums2 sorted in non - decreasing order and an integer k.
+// The median is the middle value in an ordered integer list.If the size of the list is even, there is no middle value, and the median is the mean of the two middle values.
 
-// Define a pair(u, v) which consists of one element from the first array and one element from the second array.
+// For example, for arr = [2, 3, 4], the median is 3.
+// For example, for arr = [2, 3], the median is(2 + 3) / 2 = 2.5.
+// Implement the MedianFinder class:
 
-// Return the k pairs(u1, v1), (u2, v2), ..., (uk, vk) with the smallest sums.
-
+// MedianFinder() initializes the MedianFinder object.
+// void addNum(int num) adds the integer num from the data stream to the data structure.
+// double findMedian() returns the median of all elements so far.Answers within 10 - 5 of the actual answer will be accepted.
 
 
 //     Example 1:
 
-// Input: nums1 = [1, 7, 11], nums2 = [2, 4, 6], k = 3
-// Output: [[1, 2], [1, 4], [1, 6]]
-// Explanation: The first 3 pairs are returned from the sequence: [1, 2], [1, 4], [1, 6], [7, 2], [7, 4], [11, 2], [7, 6], [11, 4], [11, 6]
-// Example 2:
+// Input
+// ["MedianFinder", "addNum", "addNum", "findMedian", "addNum", "findMedian"]
+// [[], [1], [2], [], [3], []]
+// Output
+// [null, null, null, 1.5, null, 2.0]
 
-// Input: nums1 = [1, 1, 2], nums2 = [1, 2, 3], k = 2
-// Output: [[1, 1], [1, 1]]
-// Explanation: The first 2 pairs are returned from the sequence: [1, 1], [1, 1], [1, 2], [2, 1], [1, 2], [2, 2], [1, 3], [1, 3], [2, 3]
-class MinHeap {
+// Explanation
+// MedianFinder medianFinder = new MedianFinder();
+// medianFinder.addNum(1);    // arr = [1]
+// medianFinder.addNum(2);    // arr = [1, 2]
+// medianFinder.findMedian(); // return 1.5 (i.e., (1 + 2) / 2)
+// medianFinder.addNum(3);    // arr[1, 2, 3]
+// medianFinder.findMedian(); // return 2.0
+class MedianFinder {
+    constructor() {
+        this.maxHeap = new MaxHeap(); // Max Heap for the lower half
+        this.minHeap = new MinHeap(); // Min Heap for the upper half
+    }
+
+    addNum(num) {
+        // First, add the number to the maxHeap
+        this.maxHeap.insert(num);
+
+        // Then, balance the heaps
+        // If the maxHeap's max is greater than the minHeap's min, move the max to minHeap
+        if (this.maxHeap.size() > 0 && this.minHeap.size() > 0 && this.maxHeap.peek() > this.minHeap.peek()) {
+            this.minHeap.insert(this.maxHeap.extractMax());
+        }
+
+        // Balance the sizes of the heaps, if necessary
+        if (this.maxHeap.size() > this.minHeap.size() + 1) {
+            this.minHeap.insert(this.maxHeap.extractMax());
+        } else if (this.minHeap.size() > this.maxHeap.size()) {
+            this.maxHeap.insert(this.minHeap.extractMin());
+        }
+    }
+
+    findMedian() {
+        if (this.maxHeap.size() > this.minHeap.size()) {
+            return this.maxHeap.peek();
+        } else {
+            return (this.maxHeap.peek() + this.minHeap.peek()) / 2;
+        }
+    }
+}
+
+// Helper class for MaxHeap
+class MaxHeap {
     constructor() {
         this.heap = [];
     }
 
-    push(val) {
-        this.heap.push(val);
-        this._heapifyUp();
+    size() {
+        return this.heap.length;
     }
 
-    pop() {
-        if (this.heap.length === 1) return this.heap.pop();
-        const min = this.heap[0];
+    insert(value) {
+        this.heap.push(value);
+        this.bubbleUp();
+    }
+
+    peek() {
+        return this.heap[0];
+    }
+
+    extractMax() {
+        if (this.size() === 1) return this.heap.pop();
+
+        const max = this.heap[0];
         this.heap[0] = this.heap.pop();
-        this._heapifyDown();
-        return min;
+        this.bubbleDown();
+
+        return max;
     }
 
-    _heapifyUp() {
-        let index = this.heap.length - 1;
+    bubbleUp() {
+        let index = this.size() - 1;
         while (index > 0) {
             let parentIndex = Math.floor((index - 1) / 2);
-            if (this.heap[parentIndex][0] <= this.heap[index][0]) break;
+            if (this.heap[parentIndex] >= this.heap[index]) break;
+
             [this.heap[parentIndex], this.heap[index]] = [this.heap[index], this.heap[parentIndex]];
             index = parentIndex;
         }
     }
 
-    _heapifyDown() {
+    bubbleDown() {
         let index = 0;
-        const length = this.heap.length;
+        const length = this.size();
         const element = this.heap[0];
 
         while (true) {
@@ -61,45 +114,99 @@ class MinHeap {
 
             if (leftChildIndex < length) {
                 leftChild = this.heap[leftChildIndex];
-                if (leftChild[0] < element[0]) swap = leftChildIndex;
+                if (leftChild > element) {
+                    swap = leftChildIndex;
+                }
             }
 
             if (rightChildIndex < length) {
                 rightChild = this.heap[rightChildIndex];
-                if ((swap === null && rightChild[0] < element[0]) ||
-                    (swap !== null && rightChild[0] < leftChild[0])) {
+                if (
+                    (swap === null && rightChild > element) ||
+                    (swap !== null && rightChild > leftChild)
+                ) {
                     swap = rightChildIndex;
                 }
             }
 
             if (swap === null) break;
-
-            this.heap[index] = this.heap[swap];
-            this.heap[swap] = element;
+            [this.heap[index], this.heap[swap]] = [this.heap[swap], this.heap[index]];
             index = swap;
         }
+    }
+}
+
+// Helper class for MinHeap
+class MinHeap {
+    constructor() {
+        this.heap = [];
     }
 
     size() {
         return this.heap.length;
     }
-}
 
-function kSmallestPairs(nums1, nums2, k) {
-    const result = [];
-    const heap = new MinHeap();
-
-    for (let i = 0; i < Math.min(nums1.length, k); i++) {
-        heap.push([nums1[i] + nums2[0], i, 0]);
+    insert(value) {
+        this.heap.push(value);
+        this.bubbleUp();
     }
 
-    while (heap.size() > 0 && result.length < k) {
-        const [sum, i, j] = heap.pop();
-        result.push([nums1[i], nums2[j]]);
-        if (j + 1 < nums2.length) {
-            heap.push([nums1[i] + nums2[j + 1], i, j + 1]);
+    peek() {
+        return this.heap[0];
+    }
+
+    extractMin() {
+        if (this.size() === 1) return this.heap.pop();
+
+        const min = this.heap[0];
+        this.heap[0] = this.heap.pop();
+        this.bubbleDown();
+
+        return min;
+    }
+
+    bubbleUp() {
+        let index = this.size() - 1;
+        while (index > 0) {
+            let parentIndex = Math.floor((index - 1) / 2);
+            if (this.heap[parentIndex] <= this.heap[index]) break;
+
+            [this.heap[parentIndex], this.heap[index]] = [this.heap[index], this.heap[parentIndex]];
+            index = parentIndex;
         }
     }
 
-    return result;
+    bubbleDown() {
+        let index = 0;
+        const length = this.size();
+        const element = this.heap[0];
+
+        while (true) {
+            let leftChildIndex = 2 * index + 1;
+            let rightChildIndex = 2 * index + 2;
+            let leftChild, rightChild;
+            let swap = null;
+
+            if (leftChildIndex < length) {
+                leftChild = this.heap[leftChildIndex];
+                if (leftChild < element) {
+                    swap = leftChildIndex;
+                }
+            }
+
+            if (rightChildIndex < length) {
+                rightChild = this.heap[rightChildIndex];
+                if (
+                    (swap === null && rightChild < element) ||
+                    (swap !== null && rightChild < leftChild)
+                ) {
+                    swap = rightChildIndex;
+                }
+            }
+
+            if (swap === null) break;
+            [this.heap[index], this.heap[swap]] = [this.heap[swap], this.heap[index]];
+            index = swap;
+        }
+    }
 }
